ffc8f9f0d333493752f59de16cffb8602f2512cc
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-14 16:01:03 -0400

    Fix titlebar component tests

--- src/app/shared/titlebar/titlebar.component.spec.ts
+++ src/app/shared/titlebar/titlebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TitlebarComponent } from './titlebar.component';
 
@@ -8,6 +9,9 @@ describe('TitlebarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [ TitlebarComponent ]
     })
     .compileComponents();
