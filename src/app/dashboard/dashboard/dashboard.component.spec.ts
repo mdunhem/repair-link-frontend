af94fdf9a694eef16d5b3306b11487a17fa30361
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-14 15:55:22 -0400

    Fix dashboard component tests

--- src/app/dashboard/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
 
+import { SharedModule } from "../../shared/shared.module";
+import { DashboardRoutingModule } from '../dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,6 +11,11 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        SharedModule,
+        DashboardRoutingModule
+      ],
       declarations: [ DashboardComponent ]
     })
     .compileComponents();
