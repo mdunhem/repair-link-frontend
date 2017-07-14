7454e08b5578c85c4fb5fb1676a705635fb92fac
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-14 16:01:56 -0400

    Fix vehicle detail component tests

--- src/app/vehicles/vehicle-detail/vehicle-detail.component.spec.ts
+++ src/app/vehicles/vehicle-detail/vehicle-detail.component.spec.ts
@@ -1,6 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, Subject } from "rxjs";
+import 'rxjs/observable/of';
 
 import { VehicleDetailComponent } from './vehicle-detail.component';
+import { VehiclesService } from "../shared/vehicles.service";
+import { Vehicle } from "../shared/vehicle";
+
+// Need to seperate this to a testing file or something...
+export class VehiclesTestService {
+
+  private _selectedVehicle = new Subject<Vehicle>();
+
+  public get selectedVehicle() {
+    return this._selectedVehicle.asObservable();
+  }
+
+  public getVehicle(vin: number): Observable<Vehicle> {
+    return Observable.of(new Vehicle({ vin: 0, year: 2003, make: 'Toyota', model: 'Corolla' }));
+  }
+
+  public selectVehicle(vehicle: Vehicle) {
+    this._selectedVehicle.next(vehicle);
+  }
+
+}
 
 describe('VehicleDetailComponent', () => {
   let component: VehicleDetailComponent;
@@ -8,7 +32,11 @@ describe('VehicleDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehicleDetailComponent ]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [ VehicleDetailComponent ],
+      providers: [ { provide: VehiclesService, useClass: VehiclesTestService } ]
     })
     .compileComponents();
   }));
