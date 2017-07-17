a715e0b275ca3e8b6c4f2af4fc9f0c7814267f04
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-17 00:37:49 -0400

    Remove old unused code

--- src/app/vehicles/shared/vehicles.service.ts
+++ src/app/vehicles/shared/vehicles.service.ts
@@ -1,22 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable, Subject } from "rxjs";
-import 'rxjs/observable/of';
 
 import { environment } from "../../../environments/environment";
 import { Vehicle  } from "./vehicle";
-
-let VEHICLE_UUID = 0;
-
-let VEHCILES: Array<Vehicle> = [
-  new Vehicle({_id: "1", vin: `${VEHICLE_UUID++}`, year: 2003, make: 'Toyota', vehicleModel: 'Corolla' }),
-  new Vehicle({_id: "2", vin: `${VEHICLE_UUID++}`, year: 2005, make: 'Acura', vehicleModel: 'TL' }),
-  new Vehicle({_id: "3", vin: `${VEHICLE_UUID++}`, year: 2017, make: 'Honda', vehicleModel: 'Civic' })
-];
-
-interface VehiclesMultipleAPIResponse {
-
-}
 
 @Injectable()
 export class VehiclesService {
@@ -47,13 +34,6 @@ export class VehiclesService {
       .map<any, Vehicle>((value, index) => {
         return new Vehicle(value);
       });
-    // let vehicle;
-    // VEHCILES.forEach(v => {
-    //   if (v.vin === vin) {
-    //     vehicle = v;
-    //   }
-    // });
-    // return Observable.of(vehicle);
   }
 
   public selectVehicle(vehicle: Vehicle) {
