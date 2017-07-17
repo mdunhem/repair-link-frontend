304b1eb66358aedac56fb1775920ef650243d89d
Mikael Dunhem <mikael.dunhem@gmail.com>
2017-07-17 00:46:31 -0400

    Update test with new api

--- src/app/vehicles/vehicle-detail/vehicle-detail.component.spec.ts
+++ src/app/vehicles/vehicle-detail/vehicle-detail.component.spec.ts
@@ -17,7 +17,7 @@ export class VehiclesTestService {
   }
 
   public getVehicle(vin: number): Observable<Vehicle> {
-    return Observable.of(new Vehicle({ vin: 0, year: 2003, make: 'Toyota', model: 'Corolla' }));
+    return Observable.of(new Vehicle({_id: "1", vin: "0", year: 2003, make: "Toyota", vehicleModel: "Corolla" }));
   }
 
   public selectVehicle(vehicle: Vehicle) {
